8dfde5d8e42b5d1fc1c9bafc08166d4ab086da3d
KyrylVeremiov <veremiovk@gmail.com>
2021-04-29 20:56:47 +0300

    Plot

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
-# %%
 class fa:
-    D = 10
-    t = 20
+    D = 5
+    t = 100
 
+    # D=2
+    # t=20
     size_of_population=50
     alpha=1
     B0=1
@@ -15,8 +17,6 @@ class fa:
 
 
     def initialize_population(self,a=A,b=B):
-        self.A=a
-        self.B=b
         return np.random.uniform(a, b, (self.size_of_population, self.D))
 
     def r(self,X,i,j):
@@ -29,12 +29,12 @@ class fa:
         return np.random.uniform(a, b, (1, self.D))
 
     def move_population(self,X):
-        X_new= np.zeros((self.size_of_population,self.D))
+        X_new= np.copy(X)
         for i in range(self.size_of_population):
             for j in range(self.size_of_population):
-                if self.F(X[i]>self.F(X[j])):
+                if self.F(X[i])>self.F(X[j]):
                     X_new[i]=X[j]+self.Bij(self.r(X,i,j))*(X[i]-X[j])+self.alpha*self.u(-1,1)
-                    if X_new.any()<fa.A & X_new.any()>fa.B:
+                    if X_new.any()<self.A and X_new.any()>self.B:
                         X_new[i]=X[i]
         return X_new
 
@@ -50,6 +50,7 @@ class fa:
         self.A=a
         self.B=b
         X=self.initialize_population(a,b)
+        score= list()
         if show_calculations:
             print('Initial population:')
             print(X)
@@ -64,15 +65,21 @@ class fa:
                 print('Best unit:')
                 print(self.find_best(X))
                 print('----------------')
+                score.append(self.find_best(X)[1])
         if show_calculations:
             print('\n\nFinal population')
             print(X)
             print('\nBest unit:')
             print(self.find_best(X))
+            plt.plot(np.arange(0,self.t,1),score)
+            plt.xlabel('Iterations')
+            plt.ylabel('Score of population')
+            plt.savefig('plot.png')
+            plt.show()
         return self.find_best(X)
 
+#%%
 def f(X):
     return abs(X * np.sin(X) + 0.1 * X).sum()
-#%%
-fa.solve(fa(),f,-10,10,True)
 
+fa.solve(fa(),f,-10,10,True)
